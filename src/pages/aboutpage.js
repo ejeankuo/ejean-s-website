ef77ed40b5f1d2d1cd81c7d0f6ab6a5ab7c5ee1e
Ejean Kuo <ejeankuo2027@u.northwestern.edu>
2025-09-28 15:26:46 -0500

    add desktop view of about me page

--- src/pages/aboutpage.js
+++ src/pages/aboutpage.js
@@ -1,13 +1,23 @@
-
+import "./aboutpage.css";
+import { Link } from "react-router-dom";
 
 export default function AboutPage() {
     return (
-        <div className="about-container">
-            <h1>About Me</h1>
-            <p>Hello! I'm Ejean Kuo, a second-year student at Northwestern University pursuing a B.A. in Computer Science and English Literature. I have a passion for coding, literature, and exploring the intersection of technology and creativity.</p>
-            <p>In my free time, I enjoy reading classic novels, experimenting with new programming languages, and hiking in nature. I'm always eager to learn new skills and take on exciting projects.</p>
-            <p>Feel free to explore my website to learn more about my projects, resume, and how to get in touch with me!</p>
-            <img src="/headshot.png" alt="Ejean Kuo's Headshot" className="headshot-about" />
+        <div className="about-me-header">
+            <img src="/about-me.png" alt="About me" className="signature-about" />
+            <div className="about-card">
+                <div className="about-text">
+                    <p>Hello! I'm Ejean Kuo, a third-year student at Northwestern University pursuing a B.A. in Computer Science and English Literature.</p>
+                    <p>I love to study languages––whether it's coding or reading/writing––as well as explore the intersection of technology and creativity.  I'm always eager to learn new skills and take on exciting projects.</p>
+                    <p>In my free time, I enjoy reading classic novels, experimenting with new programming languages, and café hopping.</p>
+                    <p>Feel free to explore my website to learn more about my projects and resumé. Click below to get in touch with me!</p>
+                    <Link to="/contacts" className="about-button">
+                        <img src="/connect-fruit-sticker.png" alt="Connect with me Button" className="connect-sticker" />
+                    </Link>
+                </div>
+                <img src="/busan-ejean.jpg" alt="Ejean Kuo Having Fun" className="about-photo" />
+            </div>
         </div>
+        
     );
 }
