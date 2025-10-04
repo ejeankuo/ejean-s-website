2d181f8a16d95f088999e8226bcb189ee08d4478
Ejean Kuo <ejeankuo2027@u.northwestern.edu>
2025-10-04 16:05:29 -0500

    delete old keys

--- src/App.js
+++ src/App.js
@@ -9,11 +9,6 @@ import ContactsPage from "./pages/contactspage";
 import ProjectsPage from "./pages/projectspage";
 import AboutPage from "./pages/aboutpage";
 
-// Spotify API Client ID
-var client_id = 'c20dcd145118461f81999f6cee9eea75';
-var client_secret = 'cd0994ff43504b70bf47f4d9d02f2d76';
-var redirect_uri = 'https://example.com/callback';
-
 export default function App() {
   return (
     <Router>
