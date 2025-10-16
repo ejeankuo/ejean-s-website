68fa4d307c18e4f32370bb2f32b4dbe8bf14ea52
Ejean Kuo <ejeankuo2027@u.northwestern.edu>
2025-10-16 01:02:17 -0500

    add responsivity

--- src/pages/aboutpage.js
+++ src/pages/aboutpage.js
@@ -28,6 +28,39 @@ export default function AboutPage() {
                 <img src="/in-memoriam-cover.jpg" alt="In Memoriam Book Cover" className="book-cover" />
                 <img src="/sirens-cover.jpg" alt="Sirens of Titan Book Cover" className="book-cover" />
             </div>
+            <div className="FAQ-container">
+                <h2 style={{textAlign:"center", fontSize: "28px" }}>More about me:</h2>
+                <div className="question-card">
+                    <p style={{fontWeight: "bold", fontSize: "20px", color: "#7e6072"}}>Where am I on campus?</p>
+                    <p>At Northwestern, you can find me organizing technical workshops with <span style={{fontWeight: "bold"}}>
+                    <a href="https://www.discnu.org/"> Develop + Innovate for Social Change</a></span>,
+                         working on new panel events or career talks with guest speakers and alumni.
+                    </p>
+                    <img src="/disc_catering.png" alt="Ejean with DISC exec" className="faq-photo" />
+                    <img src="/CAE_presentation.png" alt="CAE presentation" className="faq-photo" />
+                    <p> Otherwise, I'll be at Mock Trial practice, practicing my analytical and public speaking skills! 
+                        Most recently, our 
+                        <span style={{fontWeight: "bold"}}>
+                            <a href="https://www.northwesternmocktrial.org/"> Northwestern Mock Trial </a>
+                        </span> team won 8th place at Nationals. 
+                    </p>
+                    <img src="/numt_ejean.jpg" alt="Northwestern Mock Trial Defense" className="faq-photo" />
+                    <img src="/numt_fall_team.png" alt="Northwestern Mock Trial Team" className="faq-photo" />
+                </div>
+                <div className="question-card">
+                    <p style={{fontWeight: "bold", fontSize: "20px", color: "#7e6072"}}>Why software engineering?</p>
+                    <p>Growing up, I always thought I would pursue a career in law. I interned for the <span style={{fontWeight: "bold"}}><a href="https://sfdistrictattorney.org/">San Francisco District
+                        Attorney Office</a></span> as well as <span style={{fontWeight: "bold"}}><a href="https://alpha-law.co.kr/">The Alpha Law Firm</a></span> in Seoul, South Korea, which were amazing experiences.<br/>
+                        <br/>However, as I was tasked with looking through massive databases of penal codes or Supreme Court rulings, 
+                        I was constantly frustrated by how clunky it was to use these tools. The more I struggled with the user  
+                        experience of these sites, the more interested I got in the other side of things; specifically, 
+                        <span style={{fontWeight: "bold"}}>in how these tools were built, and how they could be improved.</span> <br/> 
+                        <br/>Now, I love to code because it allows me to combine my analytical skills with my creativity to build 
+                        tools that can make a difference! Check out my <Link to="/projects">projects page</Link> to see some of the things I've developed.
+                    </p>
+                    <img src="/sfda_internship.jpg" alt="SFDA BeReal Screenshot" className="faq-photo" />
+                </div>
+            </div>
         </div>
     );
     // 
